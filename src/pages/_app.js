32eeacf862dee48cce2a6897052078f5f9194236
Saúl Páez <saulpaez03@gmail.com>
2023-04-21 14:59:41 -0400

    Add metadata

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,22 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import { ThemeProvider, createTheme } from "@mui/material";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={createTheme({
-      palette:{
-        mode:"dark"
-      }
-    })}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>DAPP</title>
+      </Head>
+      <ThemeProvider
+        theme={createTheme({
+          palette: {
+            mode: "dark",
+          },
+        })}
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
